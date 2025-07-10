024cb0d848c87f2cbe30671de33d1ed4de84e9a5
SAYANTH <93028796+Sayanth8@users.noreply.github.com>
2025-07-10 23:07:12 +0530

    succesfully developed game program

--- java.js
+++ java.js
@@ -1,1 +1,55 @@
-console.log("Hello World")
+let num = prompt("Enter the number of rounds you want to play (1-5):");
+function getComputerChoice(){
+    let num=Math.random();
+    if (num > 0.6){
+        return "rock";
+    }
+    else if ( num > 0.3 ){
+        return "paper";
+    }
+    else {
+        return "scissor";
+    }
+}
+function getHumanChoice(){
+    let choice=prompt("Enter your choice:");
+    return choice;
+}
+let humenscore=0;
+let computerscore=0;
+function playround(humanchoice,computerchoice){
+    if ( humanchoice.toLowerCase()==="rock" && computerchoice==="scissor"){
+        humenscore++;
+        return("You win, Rock beats Scissors");
+        
+    }
+    else if (humanchoice.toLowerCase()==="scissor" && computerchoice==="paper"){
+        humenscore++;
+        return("You win, Scissor beats Paper");
+    }
+     else if (humanchoice.toLowerCase()==="paper" && computerchoice==="rock"){
+        humenscore++;
+        return("You win, Paper beats rock");
+    }
+    else if (humanchoice.toLowerCase()===computerchoice){
+        return("Draw");
+    }
+    else {
+        computerscore++
+        return(`You Lose, ${computerchoice} beats ${humanchoice}`)
+    }
+}
+
+for ( let i=0; i<num; i++){
+        const humanselection=getHumanChoice();
+        const computerselection=getComputerChoice();
+       alert(playround(humanselection,computerselection));
+    }
+console.log(`Human Score: ${humenscore}`);
+console.log(`Computer Score: ${computerscore}`);
+if (humenscore > computerscore){
+    alert("You win the game!");
+}
+else if (humenscore < computerscore){
+    alert("You lose the game!");
+}
